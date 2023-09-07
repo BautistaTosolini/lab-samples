584c1bd944f0de31f3bcb1e1eccf31396783eefa
Bautista <bautistatosolini@gmail.com>
2023-09-07 16:24:07 -0300

    updated API_BASE constant

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 export const COOKIE_NAME = 'AuthToken';
 export const MAX_AGE = 3 * 24 * 60 * 60;
-export const API_BASE = 'http://localhost:3000/';
+export const API_BASE = process.env.API_BASE || 'http://localhost:3000/';
